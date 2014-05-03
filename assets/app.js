3a914397340d9b1be23e6d1a70a1c41eae49974e
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-05-02 20:41:27 -0400

    implements javascript that stores a value when button is clicked

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
-	var myText = 'Are we ready':
-	console.log(myText);
+	$('#submit').click(function(event) {
+		event.preventDefault();
+			var myValue = $('#input').val();
+
+			//$($('<span>'+ myValue +'</span>')).appendTo('#item-list').addClass('item');
+			// $( ".item-clone" ).clone().appendTo( "#item-list" ).show();
+			// $('#item-list').append($('<div>' + myValue + '</div>')).addClass(item);
+				console.log(myValue);
+		// Create a New .item
+		// Get a Value From Text Input
+		// Append Value to #item-list
+	});
 });
